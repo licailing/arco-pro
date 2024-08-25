921367c42bac69562ddeeb27b6841030bf79a862
licailing <licailing1993@sina.com>
2024-08-25 09:35:11 +0800

    配置优化

--- config/vite.config.dev.ts
+++ config/vite.config.dev.ts
@@ -6,10 +6,12 @@ export default mergeConfig(
   {
     mode: 'development',
     server: {
+      host: '0.0.0.0',
+      port: 3000,
       watch: {
         usePolling: true, // 修复HMR热更新失效
       },
-      open: true,
+      open: process.platform.includes('win') ? true : false,
       fs: {
         strict: true,
       },
