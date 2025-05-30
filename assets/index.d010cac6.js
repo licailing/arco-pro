b1b6b70646ff2d03f7d362c5807b4f8d25196cc2
licailing <licailing1993@sina.com>
2025-05-30 22:11:33 +0800

    预览

---
+++ assets/index.d010cac6.js
@@ -0,0 +1,1 @@
+import{d as o,B as r,C as a}from"./arco.aca93ce0.js";import{f as s,h as n}from"./vue.7ac8ed9f.js";const i=o({__name:"index",setup(c){const e=s(),t=n().params.path;return e.replace({path:t}),(u,m)=>(r(),a("div"))}});export{i as default};
